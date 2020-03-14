8a122280b1564ef4e1fc4d88b43a9fd50aa25dab
Mikael <mikael@rosel.se>
2020-03-14 22:21:32 +0000

    Pretty ok

--- kml_to_geojson.py
+++ kml_to_geojson.py
@@ -1,14 +1,26 @@
-from os import listdir
-from zipfile import ZipFile
 import json
 import os
 import shutil
+from os import listdir
+from zipfile import ZipFile
 from shutil import copyfile
+from datetime import datetime
+from datetime import date
 
 nc_path = '/home/mikael/Nextcloud/Route/'
 kml_path = '/home/mikael/mikaelhug.github.io/kml_files/'
 data_path = '/home/mikael/mikaelhug.github.io/data.json'
 kml_files = listdir(nc_path)
+
+weekdays = {
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+}
 
 with open(data_path, 'r') as f:
     data = json.load(f)
@@ -17,6 +29,16 @@ already_added_routes = []
 for route in data['features']:
     already_added_routes.append(route['properties']['tag'])
 
+def replace_in_file(vfile, var, val):
+    with open(vfile, 'r') as f:
+        vdata = f.readlines()
+
+    for ix, line in enumerate(vdata):
+        if line.startswith('var '+var):
+            vdata[ix]= 'var '+var+' = ['+val[1]+', '+val[0]+'];'
+
+    with open(vfile, 'w') as f:
+        f.writelines(vdata)
 
 def s_turn(kmldata, splitter):
     try:
@@ -24,6 +46,15 @@ def s_turn(kmldata, splitter):
     except:
         return 0
 
+def unix2date(unixtime):
+    unixtime = float(unixtime)
+    return datetime.utcfromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
+
+def get_weekday(datestamp):
+    dt = datestamp.split()[0]
+    year, month, dag = (int(x) for x in dt.split('-'))
+    day_int = date(year, month, dag).weekday()
+    return weekdays[day_int]
 
 def all_kmz_to_kml():
     kml_files_git = listdir(kml_path)
@@ -52,7 +83,6 @@ def all_kmz_to_kml():
 
         print("Copied: "+kmlname)
 
-
 def kml_to_geo():
     kml_files_git = listdir(kml_path)
 
@@ -65,14 +95,14 @@ def kml_to_geo():
             kmldata = f.read()
 
         name = kmldata.split('<name>')[1].split('</name>')[0]
-        day = "monday"
         description = kmldata.split('<description>')[1].split('</description>')[0]
         tag = kml
         distanceTotal = s_turn(kmldata, 'distanceTotal')
         timeMoving = s_turn(kmldata, 'timeMoving')
         maxSpeed = s_turn(kmldata, 'maxSpeed')
-        createdDate = s_turn(kmldata, 'createdDate')
-        endDate = s_turn(kmldata, 'endDate')
+        createdDate = unix2date(s_turn(kmldata, 'createdDate'))
+        endDate = unix2date(s_turn(kmldata, 'endDate'))
+        day = get_weekday(createdDate)
         distanceTotalUp = s_turn(kmldata, 'distanceTotalUp')
         distanceTotalDown = s_turn(kmldata, 'distanceTotalDown')
         timeMovingDown = s_turn(kmldata, 'timeMovingDown')
@@ -125,8 +155,11 @@ def kml_to_geo():
                 "coordinates": coords
             }
         }
-
         data['features'].append(dict_add)
+
+        # update latestAdd
+        replace_in_file('vars.js','latestAdd',[coords[0][0],coords[0][1]])
+        print("Added: "+kml)
 
     with open(data_path, 'w') as f:
         json.dump(data, f)
